f142e6ee07753dfc6a652b90809e5f6959559ef8
Chris Hafey <chafey@gmail.com>
2025-03-27 08:43:15 -0500

    got working on ubuntu/gcc

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ cd build
 cmake .. \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
-    -DCMAKE_C_COMPILER=clang \
-    -DCMAKE_CXX_COMPILER=clang++ \
+   # -DCMAKE_C_COMPILER=clang \
+  #  -DCMAKE_CXX_COMPILER=clang++ \
     -DBUILD_TESTING=ON
 #make -j && ctest
 #make -j && tests/openjph/openjph-unittest
