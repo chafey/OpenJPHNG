493ec3aff23671db82ddf8ce9318c61f2e4730c4
Chris Hafey <chafey@gmail.com>
2025-03-28 15:26:19 -0500

    added clangformat as default formatter

--- src/openjph/wavelet/avx512/reversible_forward_vertical_step.h
+++ src/openjph/wavelet/avx512/reversible_forward_vertical_step.h
@@ -5,23 +5,26 @@ using namespace openjph;
 using namespace wavelet;
 namespace openjph
 {
-    class line_buf;
+class line_buf;
 
 namespace wavelet
 {
-    union lifting_step;
+union lifting_step;
 
 namespace avx512
 {
 namespace reversible
 {
 
- 
- /////////////////////////////////////////////////////////////////////////
-    void avx512_rev_vert_step32(const lifting_step* s, const line_buf* sig, 
-                                const line_buf* other, const line_buf* aug, 
-                                ui32 repeat, bool synthesis);
-    }
-}
-}
-}
+
+/////////////////////////////////////////////////////////////////////////
+void avx512_rev_vert_step32(const lifting_step *s,
+                            const line_buf *sig,
+                            const line_buf *other,
+                            const line_buf *aug,
+                            ui32 repeat,
+                            bool synthesis);
+} // namespace reversible
+} // namespace avx512
+} // namespace wavelet
+} // namespace openjph
